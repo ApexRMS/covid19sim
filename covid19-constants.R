399b7efa1105d60a28de04a850d57eb4e5993dd4
colindaniel <colin.daniel@apexrms.com>
2020-06-23 19:06:53 -0400

    2020-06-23 data/forecast update

--- covid19-constants.R
+++ covid19-constants.R
@@ -8,7 +8,7 @@ library(lubridate)
 
 # ******************* SET THIS DATE BEFORE RUNNING **************
 # This should be the day after the last death data
-# runDate = "2020-05-24"
+# runDate = as_date("2020-06-22")
 runDate = today()
 # ***************************************************************
 
